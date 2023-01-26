ee507c71037f2dcd13acd670aa33a9b38154f460
Christian_Ashley <christianashley86@gmail.com>
2023-01-26 08:57:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main-Entry point
  * Description: prints "Programming is like
- * building a multilingual puzzel"
+ * building a multilingual puzzle"
  * Return: Always 0 (Success)
  */
 int main(void)

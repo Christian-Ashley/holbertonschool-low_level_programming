88ae9e7cf05735e3ecf181dc2dfb385e1f8d31de
Christian_Ashley <christianashley86@gmail.com>
2023-02-03 14:36:51 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
+#ifndef _PUTCHAR
+#define _PUTCHAR
 
+void print_numbers(void);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);

5407c5fdb513cc2100475ed1ddb31b3cb35cedb8
Christian_Ashley <christianashley86@gmail.com>
2023-02-04 11:32:00 -0800

    running checker todebug

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints from 0 to 9 except 2 and 4
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x + '0');
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}

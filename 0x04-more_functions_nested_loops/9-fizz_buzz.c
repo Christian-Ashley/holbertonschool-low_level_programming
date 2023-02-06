2572b8a0fa5d1e08d55ff52591934b28a1e34418
Christian_Ashley <christianashley86@gmail.com>
2023-02-05 20:13:41 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,21 @@ void main(void)
 	for (x >= 1; x <= 100; x++)
 	{
 		if (x % 3 == 0)
+		{
 			printf("Fizz");
-		if (x % 5 == 0)
+		}
+		else if (x % 5 == 0)
+		{
 			printf("Buzz");
-		if (x % 3 == 0 && x % 5 == 0)
+		}
+		else if (x % 3 == 0 && x % 5 == 0)
+		{
 			printf("FizzBuzz");
-		if (x % 3 != 0 && x % 5 != 0)
+		}
+		else if (x % 3 != 0 && x % 5 != 0)
+		{
 			printf("%d", x);
-		putchar(' ');
+		}
+		printf(" ");
 	}
 }

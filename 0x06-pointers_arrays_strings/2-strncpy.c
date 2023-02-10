5be841df608c7194efb3db79a2b6478e78d40a2d
Christian_Ashley <christianashley86@gmail.com>
2023-02-10 14:09:53 -0800

    owmyhubris

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,22 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0;
+	int sl = 0;
+
+	while (src[index])
+	{
+		index++;
+		sl++;
+	}
 
 	for (index = 0; src[index] != '\0' && index < n; index++)
-
+	{
 		dest[index] = src[index];
+	}
 
+	for (index = sl; index < n; index++)
+	{
+		dest[index] = '\0';
+	}
 	return (dest);
 }

df38203d8d169aaf9dd2ee8b0def57ba90d21a26
Christian_Ashley <christianashley86@gmail.com>
2023-02-13 16:06:06 -0800

    4

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * print_chessboard - Print a whole as mf chessboard i guess
+ * @a: rows and columns in an array of pieces
+ * Return: cool ah chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, b;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[i][b]);
+		}
+		_putchar('\n');
+	}
+}

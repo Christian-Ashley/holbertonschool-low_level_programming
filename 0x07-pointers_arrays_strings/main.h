2ccce9627a746cf53526d23b3353e2397e1f45ae
Christian_Ashley <christianashley86@gmail.com>
2023-02-13 16:01:34 -0800

    3

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);

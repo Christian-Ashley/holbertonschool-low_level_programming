7f16fae8e21508ee15cba7701cc1f9f691703712
Christian_Ashley <christianashley86@gmail.com>
2023-02-15 11:50:50 -0800

    i drink to forget men like you

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,10 @@
  * Return: length of string
  */
 
-int _strlen_recursions(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
 	else
-		return (1 + _strlen_recursions(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 }

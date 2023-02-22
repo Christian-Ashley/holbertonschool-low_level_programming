9ec2c0ce5263fc997a0827bb00b242293ce0ee02
Christian_Ashley <christianashley86@gmail.com>
2023-02-22 11:31:08 -0800

    i am so tired

---
+++ 0x0A-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strcpy - do not think i have for gotten your treachery
+ * @dest: betty you will suffer
+ * @src: i am still coming for you
+ * Return: my honor
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (0);
+}

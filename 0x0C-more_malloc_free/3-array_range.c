0ae5e532786a20080689daa24bc97a929f968382
Christian_Ashley <christianashley86@gmail.com>
2023-02-27 12:41:02 -0800

    grrd

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *array_range - the range of the array
+ *@min: the min
+ *@max: the max
+ *Return: this time number
+ */
+
+int *array_range(int min, int max)
+{
+	int *a, i, len;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 1;
+
+	a = malloc(sizeof(int) * len);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		a[i] = i + min;
+
+	return (a);
+}

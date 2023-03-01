68722af9913386b9cefe9d6ecd4c6296c32365d9
Christian_Ashley <christianashley86@gmail.com>
2023-03-01 10:48:02 -0800

    asking deep questions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,19 @@
-#ifndef HEADER_H
-#define HEADER_H
-#define struct dog { char *name; float age; char *owner; };
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - release the dogs of war
+ * @name: what do you call yourself?
+ * @age: does age truly bring wisdom?
+ * @owner: are you actually doing any of this for yourself?
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
 #endif

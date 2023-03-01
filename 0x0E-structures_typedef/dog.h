4ed0f2514f9bfc36b4f87609857e3adbd5be3ba8
Christian_Ashley <christianashley86@gmail.com>
2023-03-01 07:01:46 -0800

    lets see if i got this right

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * struct dog - a structure that has info some adorable dogs
+ * @name: the dogs name
+ * @age: the dogs age
+ * @owner: the dogs pet human
+ * 
+ * Description: the information is probably fictional but the pictures they use in the questions are so cute that i dont care
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}

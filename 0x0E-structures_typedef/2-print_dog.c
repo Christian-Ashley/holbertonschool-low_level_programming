ff89c8b6ebbe64cf1d764babd45deb25d1054435
Christian_Ashley <christianashley86@gmail.com>
2023-03-04 09:50:50 -0800

    tough call

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+/**
+ * print_dog - prints structure dog
+ * @d: the variable that means structure dog
+ * Return: void
+ */
+
+void print_dog(struct dog *d);
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n);
+	else
+		printf("Name: %s\n", d->age);
+
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nill)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}

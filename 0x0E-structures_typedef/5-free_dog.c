9a4cd7e8666fa1cc1e330d8191fa8f1f435f5dcd
Christian_Ashley <christianashley86@gmail.com>
2023-03-05 14:58:33 -0800

    lets get a move on

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: dog_t pointer
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{ return; }
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}

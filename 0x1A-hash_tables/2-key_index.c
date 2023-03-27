4601797668f93cf8e414e577e5977ca34a3938d5
Christian_Ashley <christianashley86@gmail.com>
2023-03-27 09:21:28 -0700

    typo

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,7 @@
  * Return: indexed location
  */
 
-unsigned long int Key_index(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
 

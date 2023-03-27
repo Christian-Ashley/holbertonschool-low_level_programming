9f59df544671ad81ba1f63514ec2afc811955b02
Christian_Ashley <christianashley86@gmail.com>
2023-03-27 09:19:53 -0700

    3

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of the key
+ * @key: the key
+ * @size: is the size of the array of the hash table
+ * Return: indexed location
+ */
+
+unsigned long int Key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+
+	return (index % size);
+}
